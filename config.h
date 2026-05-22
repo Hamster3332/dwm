42b545ba2720f0fed4760906a6422e0acba42d9d
MaliCopter <mali.copter@gmx.de>
2026-05-22 15:58:36 +0200

    autostart cleanup

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
 /* appearance */
 static const unsigned int borderpx  = 4;   /* border pixel of windows */
 static const unsigned int snap      = 32;  /* snap pixel */
-static const unsigned int gappih    = 20;  /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;  /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;  /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;  /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;  /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;  /* vert outer gap between windows and screen edge */
 static int smartgaps                = 0;   /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;   /* 0 means no bar */
 static const int topbar             = 1;   /* 0 means bottom bar */
@@ -42,10 +42,11 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
     { "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
-    { "vesktop",  NULL,       NULL,       1 << 3,       0,           -1 },
+    { "discord",  NULL,       NULL,       1 << 3,       0,           -1 },
     { "steam",    NULL,       NULL,       1 << 4,       0,           -1 },
+    { "telegram", NULL,       NULL,       1 << 7,       0,           -1 },
+    { "signal",   NULL,       NULL,       1 << 7,       0,           -1 },
     { NULL,       NULL,       "win[0-9]+",0,            1,           -1 },
 };
 
@@ -98,11 +99,15 @@ static const char *roficmd[]    = { "rofi", "-show", "drun", "-theme", "~/.confi
 
 static const char *keepassxccmd[] = {"keepassxc", NULL};
 static const char *emacscmd[] = {"emacs", NULL};
+static const char *signalcmd[] = {"signal-desktop", NULL};
+static const char *telegramcmd[] = {"telegram", NULL};
 
 Autostarttag autostarttaglist[] = {
 	{.cmd = browsercmd, .tags = 1 << 2 },
 	{.cmd = keepassxccmd, .tags = 1 << 6 },
-	{.cmd = emacscmd, .tags = 0 },
+	{.cmd = emacscmd, .tags = 1 << 0 },
+	{.cmd = signalcmd, .tags = 1 << 7 },
+	{.cmd = telegramcmd, .tags = 1 << 7 },
 	{.cmd = NULL, .tags = 0 },
 };
 
